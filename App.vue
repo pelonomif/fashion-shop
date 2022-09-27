6d11954b5622e4f63f2655a88d5baafc3344fdd6
pelonomif <pelonomif@gmail.com>
2022-09-27 20:25:21 +0200

    Set up the files I need

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+    <router-link to="/">Home </router-link>
+</template>
+
+<script>
+
+export default {
+    name: "App", 
+    data () {
+        return {
+            products:[
+               
+            ]
+        }
+        
+    },
+}
+</script>
+<style scoped>
+
+</style>
---
+++ views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=" homepage">Home</div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    
+
+}
+</script>
+
+<style>
+
+</style>
