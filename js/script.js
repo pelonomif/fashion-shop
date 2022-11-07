fbcf25232f91cd8877b4ef4da21c859cdf14b87f
pelonomif <pelonomif@gmail.com>
2022-11-07 12:54:18 +0200

    added event listener on menu button

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.getElementById("showMenu")
+const bar = document.getElementById("bar");
+const nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", function(e){
+    console.log("clicked")
+});
