5a00c03af7a773111c6582f142f5b325b4e07e39
pelonomif <pelonomif@gmail.com>
2022-11-08 09:00:18 +0200

    created shop page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const menuBtn = document.getElementById("showMenu")
 const bar = document.getElementById("bar");
-const nav = document.querySelector("nav");
+const menu = document.getElementById("menu")
 
+//showing navbar menu when button is clicked
 menuBtn.addEventListener("click", function(e){
-    console.log("clicked")
+    if (menuBtn.clicked == true){
+        menu.style.display == "flex";
+    }
+    else{
+        menu.style.display == "none";
+    }
 });
